c9afdd60050cab390700b01a8f77636ea9d0252a
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-01-12 23:31:40 +0530

    third attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,20 +3,20 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
-        System.out.println(p.product(1,2));
-        System.out.println(p.product(1,2,3));
-        System.out.println(p.product(1.5,5.5));
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.5,5.5);
     }
 }
 
 class Product {
     public int product(int x, int y) {
-        return x;
+        return x+y;
     }
     public int product(int x, int y, int z) {
-        return x;
+        return x+y+z;
     }
     public double product(double x, double y) {
-        return x;
+        return x+y;
     }
 }
