51513c84debcb79b75394d43c1b82342fc5b1a65
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-01-13 16:12:49 +0530

    eight attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,17 +8,5 @@ public class Main {
         double k = p.product(1.5,5.5);
         System.out.println(i + " " + j + " " + k);
     }
-
-    public static class Product {
-        public int product(int x, int y) {
-            return x+y;
-        }
-        public int product(int x, int y, int z) {
-            return x+y+z;
-        }
-        public double product(double x, double y) {
-            return x+y;
-        }
-    }
 }
 
